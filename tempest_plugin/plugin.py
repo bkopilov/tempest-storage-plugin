eca2a30f2870e52d14d7450ae5a4b606eb0d19ed
Benny Kopilov <bkopilov@redhat.com>
2016-12-20 09:56:18 +0200

    Add volume base to plugin

--- tempest_plugin/plugin.py
+++ tempest_plugin/plugin.py
@@ -27,13 +27,11 @@ class StorageTempestPlugin(plugins.TempestPlugin):
     def load_tests(self):
         base_path = os.path.split(os.path.dirname(
             os.path.abspath(__file__)))[0]
-        test_dir = "tempest_plugin/tests"
+        test_dir = "tempest-storage-plugin/tempest_plugin/tests"
         full_test_dir = os.path.join(base_path, test_dir)
         return full_test_dir, base_path
 
     def register_opts(self, conf):
-        config.register_opt_group(conf, project_config.hypervisor_group,
-                                  project_config.HypervisorGroup)
-
+        pass
     def get_opt_lists(self):
         pass
