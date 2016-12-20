44ed0810d3416521b62c702692584138106bbf73
Benny Kopilov <bkopilov@redhat.com>
2016-12-20 10:01:58 +0200

    Update plugin path

--- tempest_plugin/plugin.py
+++ tempest_plugin/plugin.py
@@ -27,11 +27,12 @@ class StorageTempestPlugin(plugins.TempestPlugin):
     def load_tests(self):
         base_path = os.path.split(os.path.dirname(
             os.path.abspath(__file__)))[0]
-        test_dir = "tempest-storage-plugin/tempest_plugin/tests"
+        test_dir = "tempest_plugin/tests"
         full_test_dir = os.path.join(base_path, test_dir)
         return full_test_dir, base_path
 
     def register_opts(self, conf):
         pass
+
     def get_opt_lists(self):
         pass
