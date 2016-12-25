51820477d85a609a71dac104171c81d4efb777e2
Benny Kopilov <bkopilov@redhat.com>
2016-12-25 22:13:05 +0200

    add new clients to downstream tempest

---
+++ tempest_plugin/lib/__init__.py
@@ -0,0 +1,3 @@
+import os
+
+LIB = os.getcwd
--- tempest_plugin/tests/api/volume/base.py
+++ tempest_plugin/tests/api/volume/base.py
@@ -1,5 +1,7 @@
 
 import tempest.api.volume.base as base_tempest_volume
+from lib.services.volume.v3 import availability_zone_client
+from lib.services.volume.v3 import volumes_client
 
 
 class BaseVolumeTest(base_tempest_volume.BaseVolumeTest):
@@ -14,6 +16,19 @@ class BaseVolumeTest(base_tempest_volume.BaseVolumeTest):
     @classmethod
     def setup_clients(cls):
         super(BaseVolumeTest, cls).setup_clients()
+        cls.client_manager = cls.get_client_manager()
+        auth_provider = cls.client_manager.auth_provider
+        service_name="volume"
+        region_name = "RegionOne"
+        cls.availability_zone_client_v3 = \
+            availability_zone_client.AvailabilityZoneClient(
+                auth_provider,
+                service=service_name,
+                region=region_name)
+        cls.volumes_client_v3 = volumes_client.VolumesClient(
+            auth_provider,
+            service=service_name,
+            region=region_name)
 
     @classmethod
     def resource_setup(cls):
@@ -36,6 +51,20 @@ class BaseVolumeAdminTest(base_tempest_volume.BaseVolumeAdminTest):
     @classmethod
     def setup_clients(cls):
         super(BaseVolumeAdminTest, cls).setup_clients()
+        cls.client_manager = cls.get_client_manager()
+        auth_provider = cls.client_manager.auth_provider
+        service_name = "volume"
+        region_name = "RegionOne"
+        cls.admin_availability_zone_client_v3 = \
+            availability_zone_client.AvailabilityZoneClient(
+                auth_provider,
+                service=service_name,
+                region=region_name)
+
+        cls.admin_volumes_client_v3 = volumes_client.VolumesClient(
+            auth_provider,
+            service=service_name,
+            region=region_name)
 
     @classmethod
     def resource_setup(cls):
